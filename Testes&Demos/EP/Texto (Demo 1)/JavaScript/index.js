5c17d0427e8f4ccc7903e453e13ba8fb6d7ba5ea
Guerdem <josejefferson895@gmail.com>
2023-01-20 16:09:30 -0300

    Nova Demo de um Website de texto
    Esqueci de atualizar antes. Fazendo agora já que vou fazer uma alterações, e talvez eu mude depois.

---
+++ Testes&Demos/EP/Texto (Demo 1)/JavaScript/index.js
@@ -0,0 +1,38 @@
+var questionsSvg = document.querySelectorAll('.open-info-svg')
+
+
+/*Web Open Questions Options*/
+const questions = questionsSvg[0]
+const questions1 = questionsSvg[1]
+
+/*Web Close Questions Options*/
+const closeQ = document.querySelector('.close-extra-info')
+
+/*Extra Questions*/
+const questionsSection = document.querySelector('div.extra-questions-closed')
+
+
+
+/*Expand Extra Questions*/
+questions.addEventListener('click', () =>{
+    questionsSection.setAttribute('id', 'extra-questions-opened')
+    
+})
+
+questions1.addEventListener('click', () =>{
+    questionsSection.setAttribute('id', 'extra-questions-opened')
+})
+/*Close Extra Questions*/
+
+
+closeQ.addEventListener('click', () =>{
+    questionsSection.removeAttribute('id')
+})
+
+
+
+
+
+
+
+console.log(closeQ)
